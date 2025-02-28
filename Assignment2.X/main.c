6da6ba2d5a8a48c5d1657c672c946b35fcef82a2
FilippoGorini <filippogorini7@gmail.com>
2025-02-28 18:18:04 +0100

    UPDATE: just some explanatory comments

--- Assignment2.X/main.c
+++ Assignment2.X/main.c
@@ -1,14 +1,15 @@
 #include "timer.h"
 #include <xc.h>
 
-#define LED LATAbits.LATA0  // LD1 is connected to RA0
+// define LED variable for convenience
+#define LED LATAbits.LATA0          // LD1 is connected to RA0
 
 int main() {
-    TRISAbits.TRISA0 = 0;  // Set RA0 as output
-    tmr_setup_period(TIMER1, 200);  // Set Timer1 for 200 ms
+    TRISAbits.TRISA0 = 0;           // Set RA0 as output
+    tmr_setup_period(TIMER1, 200);  // Set Timer1 for 200 ms and start it
 
     while (1) {
-        LED = !LED;  // Toggle LED state
-        tmr_wait_period(TIMER1);  // Wait for 200 ms
+        LED = !LED;                 // Toggle LED state
+        tmr_wait_period(TIMER1);    // Wait until next 
     }
 }
--- Assignment2.X/timer.c
+++ Assignment2.X/timer.c
@@ -1,21 +1,26 @@
 #include "timer.h"
 #include <xc.h>
 
-#define FOSC 144000000  // 144 MHz system clock (Fcy = Fosc/2)
-#define FCY (FOSC / 2)  // 72 MHz instruction cycle
-#define PRESCALER 256
-#define TICKS_PER_MS (FCY / PRESCALER / 1000)  // 281.25 ticks per ms
+// The Fosc in our setup is 144 MHz. Therefore, the instruction cycle frequency
+// is Fcy = Fosc / 2 = 72 Mhz. In our application we will always use the timer 
+// in the first of the 4 functioning modes (see slide 15), which uses the 
+// peripheral clock Fp (which is the same as Fcy at 72 MHz). At 72 MHz, 200 ms
+// corresponds to 72000000 / 5 = 14400000 ticks. Therefore we need to set the 
+// prescaler to 256 in order for PR1 to be representable with 16 bits:
+// 14400000/256 = 56250 < 65535, OK
 
 void tmr_setup_period(int timer, int ms) {
-    unsigned int timer_count = (281.25 * ms);  // Convert ms to timer ticks
+    // The timer ticks in 1 ms can be calculated as:
+    // Fcy / 256 / 1000 = 281.25
+    unsigned int timer_count = (281.25 * ms);   // Convert ms to timer ticks
 
     if (timer == TIMER1) {
-        T1CONbits.TON = 0;    // Stop Timer1
-        T1CONbits.TCKPS = 3;  // Prescaler 1:256 (TCKPS: 00=1, 01=8, 10=64, 11=256)
-        PR1 = timer_count;    // Set timer period
-        TMR1 = 0;             // Reset Timer1 counter
-        IFS0bits.T1IF = 0;    // Clear Timer1 interrupt flag
-        T1CONbits.TON = 1;    // Start Timer1
+        T1CONbits.TON = 0;          // Stop Timer1
+        IFS0bits.T1IF = 0;          // Clear Timer1 interrupt flag
+        TMR1 = 0;                   // Reset Timer1 counter
+        PR1 = timer_count;          // Set timer period
+        T1CONbits.TCKPS = 3;        // Prescaler 1:256 (TCKPS: 00=1, 01=8, 10=64, 11=256)
+        T1CONbits.TON = 1;          // Start Timer1
     }
 }
 
@@ -24,10 +29,10 @@ int tmr_wait_period(int timer) {
 
     if (timer == TIMER1) {
         if (IFS0bits.T1IF) {
-            expired = 1;  // Expired before function call
+            expired = 1;            // Expired before function call
         }
-        while (!IFS0bits.T1IF);  // Wait for Timer1 flag
-        IFS0bits.T1IF = 0;       // Clear flag
+        while (!IFS0bits.T1IF);     // Wait for Timer1 flag
+        IFS0bits.T1IF = 0;          // Clear flag
     }
 
     return expired;
